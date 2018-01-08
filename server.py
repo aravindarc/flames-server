a497411d78188046c8e4cc8d7cd3dea37745daf4
Aravinda Kumar <aravindrckumar@gmail.com>
2018-01-09 02:52:01 +0530

    added two flames distribution images

--- server.py
+++ server.py
@@ -12,27 +12,40 @@ def upload_file():
 
 @app.route('/flamesEngine', methods = ['POST'])
 def flamesEngine():
-    global boyName, girlName, result
-    boyName = request.form['bn']
-    girlName = request.form['gn']
-    nameStriker = flames.NameStriker()
-    result = nameStriker.flamesCalculate(boyName, girlName)
+    if '0-25' in request.form:
+        return redirect(url_for('image1'))
+    if '0-50' in request.form:
+        return redirect(url_for('image2'))
+    if 'submit' in request.form:
+        global boyName, girlName, result
+        boyName = request.form['bn']
+        girlName = request.form['gn']
+        nameStriker = flames.NameStriker()
+        result = nameStriker.flamesCalculate(boyName, girlName)
 
-    if result == 'FRIENDS':
-        flamesResult = '%s and %s are FRIENDS' % (boyName, girlName)
-    elif result == 'LOVE':
-        flamesResult = '%s LOVES %s' % (boyName, girlName)
-    elif result == 'AFFECTIONATE':
-        flamesResult = '%s and %s are AFFECTIONATE' % (boyName, girlName)
-    elif result == 'MARRIAGE':
-        flamesResult = '%s will MARRY %s' % (boyName, girlName)
-    elif result == 'ENEMIES':
-        flamesResult = '%s and %s are ENEMIES' % (boyName, girlName)
-    elif result == 'SIBLINGS':
-        flamesResult = '%s is %s\'s SISTER' % (boyName, girlName)
-    else:
-        flamesResult = 'ERROR! CONTACT MAINTENANCE'
-    return render_template("index.html", action="Add", result=flamesResult)
+        if result == 'FRIENDS':
+            flamesResult = '%s and %s are FRIENDS' % (boyName, girlName)
+        elif result == 'LOVE':
+            flamesResult = '%s LOVES %s' % (boyName, girlName)
+        elif result == 'AFFECTIONATE':
+            flamesResult = '%s and %s are AFFECTIONATE' % (boyName, girlName)
+        elif result == 'MARRIAGE':
+            flamesResult = '%s will MARRY %s' % (boyName, girlName)
+        elif result == 'ENEMIES':
+            flamesResult = '%s and %s are ENEMIES' % (boyName, girlName)
+        elif result == 'SIBLINGS':
+            flamesResult = '%s is %s\'s SISTER' % (boyName, girlName)
+        else:
+            flamesResult = 'ERROR! CONTACT MAINTENANCE'
+        return render_template("index.html", action="Add", result=flamesResult)
+
+@app.route('/image0-25')
+def image1():
+    return render_template("image1.html")
+
+@app.route('/image0-50')
+def image2():
+    return render_template("image2.html")
 
 if __name__ == '__main__':
 	app.run(debug = True, threaded = True, host = '0.0.0.0')
